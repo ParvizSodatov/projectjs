8e36428f869d67999c5ce09617c6512b0155f6ea
Parviz04 <sodatov0705@gmail.com>
2025-04-29 20:58:27 +0300

    addSecondPage

---
+++ api.js
@@ -0,0 +1,12 @@
+import get from "./dom.js";
+let API="http://localhost:3000/data"
+async function getData() {
+    try {
+        let {data}=await axios.get(API)
+          get(data)
+          console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+export default getData
---
+++ dom.js
@@ -0,0 +1,32 @@
+let box=document.querySelector(".box")
+
+
+
+function get(data){
+    data.forEach(element => {
+        let conteiner=document.createElement("div")
+        conteiner.classList.add("conteiner")
+       let avatar=document.createElement("img")
+       avatar.src=element.avatar
+       avatar.classList.add("avatar")
+       let name=document.createElement("h1")
+       name.innerHTML=element.name
+       let sostav=document.createElement("p")
+       sostav.innerHTML=element.sostav
+       let price=document.createElement("p")
+       price.innerHTML=element.price+" сомон"
+       let checkbox=document.createElement("input")
+       checkbox.type="checkbox"
+       checkbox.checked=element.status
+         let divForButton=document.createElement("div")
+         divForButton.classList.add(".divForButton")
+let deletBtn=document.createElement("button")
+deletBtn.classList.add("fa-solid","fa-trash","deletBtn")
+let editBtn=document.createElement("button")
+editBtn.classList.add("fa-solid","fa-pen-to-square","editBtn")
+divForButton.append(deletBtn,editBtn,checkbox)
+conteiner.append(avatar,name,sostav,price,divForButton)
+        box.appendChild(conteiner)
+    });
+}
+export default get;
